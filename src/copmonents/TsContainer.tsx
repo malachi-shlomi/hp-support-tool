7f3ce7cfd4d9b34ecbd76c6354fe1e327cdc22e6
Malachi Shlomi <malachistd@gmail.com>
2024-01-21 16:44:48 +0200

    cmt

---
+++ src/copmonents/TsContainer.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function TsContainer() {
+
+    return (
+        <div className="pd-30">
+            <div className="alert alert-success">
+                <h1>Typescript</h1>
+            </div>
+        </div>
+    );
+}
+
+export default TsContainer;
